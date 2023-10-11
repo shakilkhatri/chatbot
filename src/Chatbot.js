82cdf8f2abbf6a5328d8b3c06c911ad52f896125
shakilkhatri <43198367+shakilkhatri@users.noreply.github.com>
2023-10-11 16:11:44 +0530

    Update Chatbot.js

--- src/Chatbot.js
+++ src/Chatbot.js
@@ -33,7 +33,7 @@ const Chatbot = () => {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        Authorization: `Bearer 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`
+        Authorization: `Bearer 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`
       },
       body: JSON.stringify({
         model: "gpt-3.5-turbo",
