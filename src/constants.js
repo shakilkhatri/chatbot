4883d24a0ee21af14d0144f89191bc0dd06fa1d6
Shakil khatri <khatri.shakilamd@gmail.com>
2024-05-14 22:56:25 +0530

    bug fixed

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,4 @@
-export const models = [
-  "gpt-3.5-turbo-1106",
-  "gpt-4-1106-preview",
-  "gpt-4-0613",
-];
+export const models = ["gpt-3.5-turbo-1106", "gpt-4-turbo", "gpt-4o"];
 
 export const costPer1000Tokens = [
   {
