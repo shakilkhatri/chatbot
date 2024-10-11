e98219fd8d7949a4a1146e735207ae3d08e2b534
Shakil khatri <khatri.shakilamd@gmail.com>
2024-10-11 10:29:56 +0530

    key input page removed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import Chatbot from "./Chatbot";
 import { useEffect, useState } from "react";
-import APIKeyInputPage from "./APIKeyInputPage";
 
 const App = () => {
-  const [hasAccess, setHasAccess] = useState(false);
   const [key, setKey] = useState(null);
 
-  useEffect(() => {
-    const apiKeyFromLocalStorage = localStorage.getItem("aik");
-    if (apiKeyFromLocalStorage) {
-      setHasAccess(true);
-      setKey(apiKeyFromLocalStorage);
-    }
-  }, []);
-
-  return hasAccess ? (
-    <Chatbot apikey={key} />
-  ) : (
-    <APIKeyInputPage setHasAccess={setHasAccess} />
-  );
+  return <Chatbot apikey={key} />
 };
 
 export default App;
